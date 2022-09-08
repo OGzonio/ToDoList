6f04ab5df271e559434a33fe6a89a89c91a26036
OGzonio <111788706+OGzonio@users.noreply.github.com>
2022-09-08 21:38:12 +0200

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.addEventListener('load', ()=>{
     const addNote = document.querySelector('#noteButton');
     notesUpdateAll()
 
-    addNote.addEventListener('click', ()=>{
+    addNote.addEventListener('click', (e)=>{
         notes = JSON.parse(localStorage.getItem('NotesStorage')) || [];
         const note = {
             noteText: document.getElementById('note').value,
@@ -20,28 +20,31 @@ window.addEventListener('load', ()=>{
 })
 
 function notesUpdateAll(){
+        document.getElementById('note').value=''
         document.getElementById('notesList').innerHTML ='';
         notes.forEach(note => {
             const node = document.createElement("div");
             const content = document.createElement('div');
             const actionDiv = document.createElement('div');
             document.getElementById("notesList").appendChild(node);
+            
             const edit = document.createElement('button');
 		    const deleteButton = document.createElement('button');
             edit.innerHTML = 'Edit';
 		    deleteButton.innerHTML = 'Delete';
-            content.innerHTML= `<input type="text" value="${note.noteText}" readonly class='Notetext'> `;
+            content.innerHTML= `<input type="text" value="${note.noteText}" readonly class='Notetext' id = "text"> `;
             node.appendChild(content);
             node.appendChild(actionDiv);
-            actionDiv.appendChild(edit);
+            
+            
             
 		    actionDiv.appendChild(deleteButton);
             node.classList.add('contentDiv');
             content.classList.add('NoteTextDiv');
             actionDiv.classList.add('ButtonsDiv');
             deleteButton.classList.add('deletebtn');
-            edit.classList.add('editbtn');
-
+            
+            
             deleteButton.addEventListener('click', e=>{
                 notes = notes.filter(e => e != note);
                 localStorage.setItem('NotesStorage', JSON.stringify(notes));
@@ -49,9 +52,9 @@ function notesUpdateAll(){
 
 
             })
-
-                
+           
             
+           
             
             
         })
